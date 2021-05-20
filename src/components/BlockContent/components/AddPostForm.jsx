8165cd6734ff06dc9d0b097a0e2c0803be2b3850
GorohovDmitriy <dmitriy2893@gmail.com>
2021-05-20 14:48:02 +0300

    Сделал форму упровляемой

--- src/components/BlockContent/components/AddPostForm.jsx
+++ src/components/BlockContent/components/AddPostForm.jsx
@@ -1,25 +1,64 @@
 import './AddPostForm.css';
 import HighlightOffRoundedIcon from '@material-ui/icons/HighlightOffRounded';
+import { Component } from 'react';
 
-export const AddPostForm = ({ handleAddFormHide }) => {
-	return (
-		<>
-			<from action="" className="form">
-				<button onClick={handleAddFormHide} className="close">
-					<HighlightOffRoundedIcon />
-				</button>
-				<h2>Post creation</h2>
-				<div>
-					<input className="addFormInput" type="text" name="postTitle" placeholder="Post title"></input>
-				</div>
-				<div>
-					<textarea className="addFormInput" name="postDescription" placeholder="Post description" />
-				</div>
-				<div>
-					<button onClick={handleAddFormHide} className="blackBtn" type="button">Add post</button>
-				</div>
-			</from>
-			<div className="overlay"></div>
-		</>
-	)
+export class AddPostForm extends Component {
+	state = {
+		postTitle: '',
+		postDescription: '',
+	}
+
+	handlePostTitleChange = (e) => {
+		this.setState({
+			postTitle: e.target.value
+		})
+	}
+
+	handlePostDescChange = e => {
+		this.setState({
+			postDescription: e.target.value
+		})
+	}
+
+	render() {
+		return (
+			<>
+				<form action="" className="form">
+					<button
+						onClick={this.handleAddFormHide}
+						className="close">
+						<HighlightOffRoundedIcon />
+					</button>
+					<h2>Post creation</h2>
+					<div>
+						<input className="addFormInput"
+							type="text"
+							name="postTitle"
+							placeholder="Post title"
+							value={this.state.postTitle}
+							onChange={this.handlePostTitleChange}
+						>
+						</input>
+					</div>
+					<div>
+						<textarea className="addFormInput"
+							name="postDescription"
+							placeholder="Post description"
+							value={this.state.postDescription}
+							onChange={this.handlePostDescChange}
+						/>
+					</div>
+					<div>
+						<button
+							onClick={this.handleAddFormHide}
+							className="blackBtn"
+							type="button">
+							Add post
+						</button>
+					</div>
+				</form>
+				<div onClick={this.handleAddFormHide} className="overlay"></div>
+			</>
+		)
+	}
 }
